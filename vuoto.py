58319e687140b3f4dc685bbf66fa5fbfefb00b70
net <net@SIS-08.igalvani.local>
2024-11-14 08:50:10 +0100

    punto 4

--- vuoto.py
+++ vuoto.py
@@ -4,3 +4,19 @@ def Aggiungi_Lista(lista):
     lista.append(prodotto)
     print (lista)
 Aggiungi_Lista(lista)
+Aggiungi_Lista(lista)
+Aggiungi_Lista(lista)
+
+
+
+def Stampa_Lista(lista):
+    print("lista della spesa:")
+    for i in range(len(lista)):
+        print(f"{i + 1}. {lista[i]}")
+
+def Rimuovi_Prodotto(lista):
+    prodotto = input("prodotto da rimuovere:")
+    lista.pop(lista.index(prodotto))
+
+Rimuovi_Prodotto(lista)
+Stampa_Lista(lista)
