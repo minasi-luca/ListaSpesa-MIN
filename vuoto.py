0b984b63a158d91460c0fc83e90a8a6f48510127
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2024-11-14 08:33:46 +0000

    finito tutto

--- vuoto.py
+++ vuoto.py
@@ -3,9 +3,7 @@ def Aggiungi_Lista(lista):
     prodotto = input("inserisci il prodotto:")
     lista.append(prodotto)
     print (lista)
-Aggiungi_Lista(lista)
-Aggiungi_Lista(lista)
-Aggiungi_Lista(lista)
+
 
 
 
@@ -18,10 +16,32 @@ def Rimuovi_Prodotto(lista):
     prodotto = input("prodotto da rimuovere:")
     lista.pop(lista.index(prodotto))
 
-Rimuovi_Prodotto(lista)
-Stampa_Lista(lista)
+def Conta_Elementi(lista):
+    return print("numero elementi:", len(lista))
+def Svuota_Lista(lista):
+    lista.clear()
 
 while True:
-    scelta=input("scegli cosa vuoi fare. (aggiungi , rimuovi , stampa, esci):/n 1. Aggiungi/n 2. rimuovi/n 3. stampa/n 4. esci")
+    scelta=input("scegli cosa vuoi fare. (aggiungi , rimuovi , stampa, esci):\n 1. Aggiungi\n 2. rimuovi\n 3. stampa\n 4. esci\n 5. conta elementi\n 6. svuota lista\n")
     if scelta == "1":
         Aggiungi_Lista(lista)
+
+    if scelta == "2":
+        Rimuovi_Prodotto(lista)
+    
+    if scelta == "3":
+        Stampa_Lista(lista)
+
+    if scelta == "4":
+       break;
+    
+    if scelta == "5":
+        Conta_Elementi(lista)
+
+    if scelta == "6":
+        Svuota_Lista(lista)
+
+
+
+
+    
